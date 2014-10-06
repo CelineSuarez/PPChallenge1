6424b62befaa8360882d6d33461265ae8e6aecd1
Celine <celinesuarez7@gmail.com>
2014-10-06 18:08:17 -0500

    added function

--- 99bottles/99bottles.js
+++ 99bottles/99bottles.js
@@ -1,8 +1,14 @@
 var limit = prompt("How many bottles of beer are on the wall?")
 
 function chorus(number){
-	return (number + " of bottles of beer on the wall, " + number + " of bottles of beer!  Take one down, pass it around, " + (number-1) + "bottles of beer on the wall!")
+	console.log( (number + " of bottles of beer on the wall, " + number + " of bottles of beer!  Take one down, pass it around, " + (number-1) + " bottles of beer on the wall!"))
 }
-for(i = limit; i >= 0; i--){
-	chorus(i);
+function specialchorus(smalln){
+			console.log( smalln + " bottle of beer on the wall, " + smalln + " bottle of beer! Take it down, pass it around " + (smalln-1) + " bottle of beer on the wall.");
+		}
+for(i = limit; i > 0; i--){
+	if (i <=1) {
+		specialchorus(i);	
+		      }
+	else chorus(i);
 }
