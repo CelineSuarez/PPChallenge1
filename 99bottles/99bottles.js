4e8f7ec9df9c098f6ea8529af5843ff3232728d3
Celine <celinesuarez7@gmail.com>
2014-10-06 17:44:13 -0500

    base code

--- 99bottles/99bottles.js
+++ 99bottles/99bottles.js
@@ -1,1 +1,8 @@
-//sample sdfdsfds
+var limit = prompt("How many bottles of beer are on the wall?")
+
+function chorus(number){
+	return (number + " of bottles of beer on the wall, " + number + " of bottles of beer!  Take one down, pass it around, " + (number-1) + "bottles of beer on the wall!")
+}
+for(i = limit; i >= 0; i--){
+	chorus(i);
+}
