e137759e3c64fc3752a6aad4305a5d7eeeda4579
Celine <celinesuarez7@gmail.com>
2014-10-10 15:32:46 -0500

    finished game

--- highlowgame/highlowgame.js
+++ highlowgame/highlowgame.js
@@ -1,1 +1,27 @@
-//js high low game
+$(document).ready(function(){
+	var answer = (Math.floor(Math.random() * 100) + 1);
+	var guesses = 0;
+	var num_max_guesses = 4;
+	$("#submit1").click(function(){
+		var num = $("#number").val();
+		
+		if (num < answer){
+			$("#output").html("higher");
+			guesses += 1;
+				if (guesses >= num_max_guesses){
+					$("#output").append("\nYou lose.");
+				}
+		}
+		else if (num > answer){
+			$("#output").html("lower");
+			guesses += 1;
+				if (guesses >= num_max_guesses){
+					$("#output").append("\nNERP. You lose.");
+				}
+		}
+		else {
+			$("#output").html("Correct!\nYou used " + guesses + " amount of guesses.");
+		}
+
+	});
+});
